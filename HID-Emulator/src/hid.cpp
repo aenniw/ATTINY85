86730ed1c209ba8f756cfdbc390f21c799163c0c
Martin Mihálek <aenniw@gmail.com>
2017-05-21 22:24:38 +0200

    Initial AT handler implementation
    - AT parsing done/ need to reimplement to reduce size
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- HID-Emulator/src/hid.cpp
+++ HID-Emulator/src/hid.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
 #include <SoftSerial_INT0.h>
-#include <DigiKeyboard.h>
 
-typedef enum {
-    K_PRINT, K_DELAY, K_PRESS, K_STROKE
-} HID_EVENT_TYPE;
+#define __KEYBOARD__
+
+#ifdef __KEYBOARD__
+#include <DigiKeyboard.h>
+#endif
+#ifdef __MOUSE__
+#include <DigiMouse.h>
+#endif
+
+#define AT_OK "AT->OK"
+#define AT_NOK "AT->NOK"
+#define AT_NA "AT->NA"
+#define AT_B "AT->BUSY"
 
 #define P_RX 2                        // Reception PIN (SoftSerial)
 #define P_TX 1                        // Transmition PIN (SoftSerial)
+
+
+typedef enum {
+    NOP = 0,
+    K_DELAY = 1, K_PRESS = 2, K_STROKE = 3, K_PRINT = 4,
+    M_DELAY = 11, M_PRESS = 12, M_STROKE = 13, M_MOVE = 14, M_SCROLL = 15
+} AT_TYPE;
 
 SoftSerial serial(P_RX, P_TX);           // Software serial port for control the Bluetooth module
 
@@ -15,63 +31,152 @@ void setup() {
     serial.begin(9600); // Initialize the serial port
 }
 
-void loop() {
-    DigiKeyboard.update();
+int readString(String &s, const char end = '\0') {
+    char c, len = 0;
     while (serial.available()) {
-        char c = (char) serial.read();
-        serial.print(c);
+        c = (char) serial.read();
+        if (c == end) {
+            break;
+        }
+        s += c;
+        len++;
     }
+    return len;
 }
 
-// l = buffer len | strlen(s) + 1
-int parse_int(const char *s, const uint8_t l) {
-    uint8_t i;
-    // end if c out of range 48 - 57
-    for (i = 0; i < l && s[i] >= '0' && s[i] <= '9'; i++);
-    int type = -1, exp = 1;
-    for (; i > 0; i--) {
-        if (exp == 1) type = 0;
-        type += (s[i - 1] - 48) * exp;
-        exp *= 10;
+int parse_uint(const String &msg) {
+    const int msg_len = msg.length();
+    int i;
+    for (i = msg.indexOf('+') + 1; i < msg_len; i++) {
+        msg.charAt((unsigned int) i);
     }
-    return type;
+
+    return -1;
 }
 
-// l = buffer len | strlen(s) + 1
-int get_next_arg(const char *s, const uint8_t l) {
-    int p;
-    for (p = 0; p < l && s[p] != ' '; p++);
-    return p;
-}
 
-void recieve(const char *data, uint8_t len) {
-    const HID_EVENT_TYPE type = (HID_EVENT_TYPE) parse_int(data, len + 1);
-    const int arg1 = get_next_arg(data, len), arg2 = arg1 + get_next_arg(data + arg1, len - arg1);
-    DigiKeyboard.sendKeyStroke(0);
+const char *handleAT(const String &msg) {
+    if (!msg.startsWith("AT+")) {
+        return AT_NOK;
+    }
+    const AT_TYPE type = (const AT_TYPE) parse_uint(msg);
+    const int p_start = msg.indexOf(' ');
+    if (p_start <= 0) return AT_NOK;
+    const String payload = msg.substring((unsigned int) p_start + 1).trim();
+    int arg1 = -1, arg2 = -1;
     switch (type) {
+#ifdef __KEYBOARD__
         case K_PRINT:
-            DigiKeyboard.println(data + arg1);
-            break;
+            //DigiKeyboard.println(payload);
+            return AT_OK;
         case K_DELAY:
-            DigiKeyboard.delay(parse_int(data + arg1, len - arg1));
-            break;
+            arg1 = parse_uint(payload);
+            if (arg1 < 0) return AT_NOK;
+            //DigiKeyboard.delay(arg1);
+            return AT_OK;
         case K_PRESS:
-            if (arg1 == arg2) {
-                DigiKeyboard.sendKeyPress((byte) parse_int(data + arg1, len - arg1));
-            } else {
-                DigiKeyboard.sendKeyPress((byte) parse_int(data + arg1, len - arg1),
-                                          (byte) parse_int(data + arg2, len - arg2));
-            }
-            break;
+            arg1 = parse_uint(payload),
+                    arg2 = parse_uint(payload.substring((unsigned int) (payload.indexOf(' ') + 1)));
+            if (arg1 < 0) return AT_NOK;
+            if (arg2 < 0) DigiKeyboard.sendKeyPress((byte) arg1);
+            else DigiKeyboard.sendKeyPress((byte) arg1, (byte) arg2);
+            return AT_OK;
         case K_STROKE:
-            if (arg1 == arg2) {
-                DigiKeyboard.sendKeyStroke((byte) parse_int(data + arg1, len - arg1));
-            } else {
-                DigiKeyboard.sendKeyStroke((byte) parse_int(data + arg1, len - arg1),
-                                           (byte) parse_int(data + arg2, len - arg2));
-            }
-            break;
-        default:
-            break;
+            arg1 = parse_uint(payload),
+                    arg2 = parse_uint(payload.substring((unsigned int) (payload.indexOf(' ') + 1)));
+            if (arg1 < 0) return AT_NOK;
+            if (arg2 < 0) DigiKeyboard.sendKeyStroke((byte) arg1);
+            else DigiKeyboard.sendKeyStroke((byte) arg1, (byte) arg2);
+            return AT_OK;
+#endif
+#ifdef __MOUSE__
+        case M_DELAY:
+            return AT_OK;
+        case M_PRESS:
+            return AT_OK;
+        case M_STROKE:
+            return AT_OK;
+        case M_MOVE:
+            return AT_OK;
+        case M_SCROLL:
+            return AT_OK;
+#endif
     }
+    return AT_NA;
 }
+
+void loop() {
+    String msg;
+    if (readString(msg, '\n')) {
+        serial.print("Recieved: ");
+        serial.println(msg);
+        serial.println(handleAT(msg.trim()));
+    }
+#ifdef __KEYBOARD__
+    DigiKeyboard.update();
+#endif
+#ifdef __MOUSE__
+    DigiMouse.update();
+#endif
+}
+
+//// UI ------------------------------------------------------------------------
+// > [MODE=03/K_PRINT] [ARGS...]
+// > K_STROKE T L_CTRL+L_SHIFT
+// > echo "Hello World"   <-->   K_PRINT echo "Hello World"
+
+// Web UI -> ws->stream cmds to esp -> stream ATs via i2c to ATtiny -> execute AT
+// Web UI <- ws->stream resp from esp <- stream ATs via i2c from ATtiny <- AT response
+// Web UI -> Status / Administration / HID / Logout
+// Web UI -> HID -> cmd log, cmd input, send cmd
+
+
+//// KEYBOARD ------------------------------------------------------------------
+
+// AT+01 -> STROKE
+// AT+01 [KEY] [?MODIFICATOR]
+// AT+01 (uint) (uint)
+// AT+01 parse_int() ?parse_int()
+
+// AT+02 -> PRESS
+// AT+02 [KEY] [?MODIFICATOR]
+// AT+02 (uint) (uint)
+// AT+02 parse_int() ?parse_int()
+
+// AT+03 -> DELAY
+// AT+03 [TIME]
+// AT+03 (uint)
+// AT+03 parse_int()
+
+// AT+04 -> PRINT
+// AT+04 [MSG]
+// AT+04 (char*)
+// AT+04 reuse_rest_of_buffer()
+
+
+//// MOUSE ---------------------------------------------------------------------
+
+// AT+11 -> STROKE
+// AT+11 [KEY]
+// AT+00 (uint)
+// AT+11 parse_int()
+
+// AT+12 -> PRESS
+// AT+12 [KEY]
+// AT+00 (uint)
+// AT+12 parse_int()
+
+// AT+13 -> DELAY
+// AT+13 [TIME]
+// AT+00 (uint)
+// AT+13 parse_int()
+
+// AT+14 -> MOVE
+// AT+14 [X] [Y]
+// AT+00 (int) (int)
+// AT+14 parse_int() parse_int()
+
+// AT+15 -> SCROLL
+// AT+15 [S]
+// AT+00 (int)
+// AT+15 parse_int()
