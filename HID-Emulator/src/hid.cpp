6faef0413cdfd75116514afdb09c6ddd1bcfe8c3
Martin Mihálek <aenniw@gmail.com>
2017-05-28 15:25:23 +0200

    Remove serial buffering
    - Serial buffering for AT validation caused too much overhead
      and USB leibs does not have enought time for their callback,
      thus sopeed working
    - Removed AT validation
    - Using format of msg [len][type][payload]
    
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- HID-Emulator/src/hid.cpp
+++ HID-Emulator/src/hid.cpp
@@ -1,111 +1,112 @@
 #include <Arduino.h>
 #include <SoftSerial_INT0.h>
 
-#define __KEYBOARD__
-#define __DEBUG__
-
 #ifdef __KEYBOARD__
 #include <DigiKeyboard.h>
 #endif
 #ifdef __MOUSE__
 #include <DigiMouse.h>
+const PROGMEM char usbHidReportDescriptor[] = { /* USB report descriptor */
+        0x05, 0x01,                                         // USAGE_PAGE (Generic Desktop)
+        0x09, 0x02,                                         // USAGE (Mouse)
+        0xa1, 0x01,                                         // COLLECTION (Application)
+        0x09, 0x01,                                         //		USAGE_PAGE (Pointer)
+        0xa1, 0x00,                                         //		COLLECTION (Physical)
+        0x05, 0x09,                                         //		USAGE_PAGE (Button)
+        0x19, 0x01,                                         //		USAGE_MINIMUM (Button 1)
+        0x29, 0x03,                                         //		USAGE_MAXIMUM (Button 3)
+        0x15, 0x00,                                         //		LOGICAL_MINIMUM (0)
+        0x25, 0x01,                                         //		LOGICAL_MAXIMUM (1)
+        0x95, 0x03,                                         //		REPORT_COUNT (3)
+        0x75, 0x01,                                         //		REPORT_SIZE (1)
+        0x81, 0x02,                                         //		INPUT (Data,Var,Abs)
+        0x95, 0x01,                                         //		REPORT_COUNT (1)
+        0x75, 0x05,                                         //		REPORT_SIZE (5)
+        0x81, 0x01,                                         //		Input(Cnst)
+        0x05, 0x01,                                         //		USAGE_PAGE(Generic Desktop)
+        0x09, 0x30,                                         //		USAGE(X)
+        0x09, 0x31,                                         //		USAGE(Y)
+        0x15, 0x81,                                         //		LOGICAL_MINIMUM (-127)
+        0x25, 0x7f,                                         //		LOGICAL_MAXIMUM (127)
+        0x75, 0x08,                                         //		REPORT_SIZE (8)
+        0x95, 0x02,                                         //		REPORT_COUNT (2)
+        0x81, 0x06,                                         //		INPUT (Data,Var,Rel)
+        0x09, 0x38,                                            //	 Usage (Wheel)
+        0x95, 0x01,                                            //	 Report Count (1),
+        0x81, 0x06,                                            //	 Input (Data, Variable, Relative)
+        0xc0,                                                        // END_COLLECTION
+        0xc0                                                     // END_COLLECTION
+};
 #endif
 
-#define AT_OK "AT->OK"
-#define AT_NOK "AT->NOK"
-#define AT_NA "AT->NA"
-#define AT_EOB "AT->EOB"
-
 #define P_RX 2                        // Reception PIN (SoftSerial)
-#define P_TX 1                        // Transmition PIN (SoftSerial)
-#define BUFFER_MAX 256
+#define P_TX 1                        // Transition PIN (SoftSerial)
 
 typedef enum {
     NOP = 0,
-    K_DELAY = 1, K_PRESS = 2, K_STROKE = 3, K_PRINT = 4,
-    M_DELAY = 11, M_PRESS = 12, M_STROKE = 13, M_MOVE = 14, M_SCROLL = 15
-} AT_TYPE;
-
-SoftSerial serial(P_RX, P_TX);           // Software serial port for control the Bluetooth module
-
-void setup() {
-    serial.begin(9600); // Initialize the serial port
-}
-
-static int parse_uint(const char *msg, uint8_t *offset) {
-    for (; *offset < BUFFER_MAX && (msg[*offset] > '9' || msg[*offset] < '0'); (*offset)++);
-    const int number = atoi(msg + *offset);
-    for (; *offset < BUFFER_MAX && msg[*offset] <= '9' && msg[*offset] >= '0'; (*offset)++);
-    return number;
-}
-
-static const char *handleAT(const char *msg) {
-    if (msg[0] == 'A' && msg[1] == 'T' && msg[2] == '+') {
-        return AT_NOK;
-    }
-    uint8_t offset = 3;
-    const AT_TYPE type = (const AT_TYPE) parse_uint(msg, &offset);
-    const int arg1 = parse_uint(msg, &offset), arg2 = parse_uint(msg, &offset);
-    switch (type) {
 #ifdef __KEYBOARD__
-        case K_PRINT:
-            DigiKeyboard.println(msg + offset);
-            return AT_OK;
-        case K_DELAY:
-            if (arg1 <= 0) return AT_NOK;
-            DigiKeyboard.delay(arg1);
-            return AT_OK;
-        case K_PRESS:
-            if (arg1 <= 0) return AT_NOK;
-            DigiKeyboard.sendKeyPress((byte) arg1, (byte) arg2);
-            return AT_OK;
-        case K_STROKE:
-            if (arg1 <= 0) return AT_NOK;
-            DigiKeyboard.sendKeyStroke((byte) arg1, (byte) arg2);
-            return AT_OK;
+    K_PRINT = 1, K_DELAY = 2, K_PRESS = 3, K_STROKE = 4
 #endif
 #ifdef __MOUSE__
-        case M_DELAY:
-            return AT_OK;
-        case M_PRESS:
-            return AT_OK;
-        case M_STROKE:
-            return AT_OK;
-        case M_MOVE:
-            return AT_OK;
-        case M_SCROLL:
-            return AT_OK;
+    M_MOVE = 11, M_DELAY = 12, M_PRESS = 13, M_CLICK = 14, M_SCROLL = 15
 #endif
-    }
-    return AT_NA;
+} AT_TYPE;
+
+SoftSerial serial(P_RX, P_TX);
+
+void setup() {
+    serial.begin(9600);
+#ifdef __MOUSE__
+    DigiMouse.begin();
+#endif
 }
 
-static char buffer[BUFFER_MAX] = {'\0'} , buffer_pos = 0;
-static bool buffer_ready = false;
+uint8_t l = 0, t = 0;
 
 void loop() {
-    if(buffer_ready) {
-#ifdef __DEBUG__
-        serial.print("Recieved: ");
-        serial.println((const char*) buffer);
-#else
-        handleAT(buffer);
+    if (serial.available()) {
+        if (!l) {
+            l = (uint8_t) serial.read();
+            t = NOP;
+        } else if (!t) t = (uint8_t) serial.read();
+        else {
+            const uint8_t p = (uint8_t) serial.read();
+#ifdef __KEYBOARD__
+            // If not using plentiful DigiKeyboard.delay(), make sure to call
+            // DigiKeyboard.update() at least every 50ms
+            DigiKeyboard.sendKeyStroke(0);
+            if (t == K_PRINT) {
+                DigiKeyboard.print((const char) p);
+            } else if (t == K_DELAY) {
+                DigiKeyboard.delay(p);
+            } else if (t == K_PRESS) {
+                if (l-- > 1) DigiKeyboard.sendKeyPress(p, (uint8_t) serial.read());
+                else DigiKeyboard.sendKeyPress(p);
+            } else if (t == K_STROKE) {
+                if (l-- > 1) DigiKeyboard.sendKeyStroke(p, (uint8_t) serial.read());
+                else DigiKeyboard.sendKeyStroke(p);
+            } else serial.read();
 #endif
-        buffer_ready = false;
-        buffer_pos = 0;
-    } else if (serial.available()){
-        if(buffer_pos >= BUFFER_MAX){
-            serial.println((const char*)AT_EOB);
-            buffer_ready = true;
-        } else {
-            char c = (char) serial.read();
-            if (c == '\n') {
-                buffer_ready = true;
-                buffer[buffer_pos] = '\0';
-            } else {
-                buffer[buffer_pos++] = c;
-            }
+#ifdef __MOUSE__
+            // If not using plentiful DigiMouse.delay(), make sure to call
+            // DigiMouse.update() at least every 50ms
+            if (t == M_DELAY) {
+                DigiMouse.delay(p);
+            } else if (t == M_MOVE) {
+                if (l-- > 1) DigiMouse.move((char) p, (char) serial.read(), 0);
+                else DigiMouse.move((char) p, 0, 0);
+            } else if (t == M_SCROLL) {
+                DigiMouse.scroll((char) p);
+            } else if (t == M_PRESS) {
+                DigiMouse.setButtons((unsigned char) p);
+            } else if (t == M_CLICK) {
+                if (p == 0) DigiMouse.leftClick();
+                else if (p == 1) DigiMouse.middleClick();
+                else if (p == 2) DigiMouse.rightClick();
+            } else serial.read();
+#endif
         }
+        if (l)l--;
     }
 #ifdef __KEYBOARD__
     DigiKeyboard.update();
