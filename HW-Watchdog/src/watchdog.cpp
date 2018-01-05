c108810a83616631ac144ed466f1617418886e90
Martin Mihálek <aenniw@gmail.com>
2018-01-05 02:05:18 +0100

    Updated HW-watchdog fuctionality
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- HW-Watchdog/src/watchdog.cpp
+++ HW-Watchdog/src/watchdog.cpp
@@ -3,97 +3,94 @@
 
 #define STREAM DigiUSB
 
-#define LED_PIN     15
-#define RESET_PIN   17
-#define DEF_DELAY   60
-#define TO_SECONDS  1000
+#define D0          0
+#define D1          1
+#define D2          2
+#define D3          3
+#define D4          4
+#define D5          5
 
-unsigned long startTime = 0, lastPing = 0, parmDelay = DEF_DELAY * TO_SECONDS;
+#define LED_PIN         D1
+#define RESET_PIN       D0
+#define TOGGLE_DELAY    1000
+
+unsigned long startTime = 0, lastPing = 0, parmDelay = 60000;
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);
     pinMode(RESET_PIN, OUTPUT);
+    digitalWrite(RESET_PIN, HIGH);
     STREAM.begin();
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
 }
 
-static char read_char() {
-    char lastRead = '\0';
-    if (STREAM.available()) {
-        lastRead = static_cast<char>(STREAM.read());
+static void toggle(const uint8_t pin, unsigned long delay_time) {
+    digitalWrite(pin, LOW);
+    STREAM.delay(delay_time);
+    digitalWrite(pin, HIGH);
+}
+
+const uint8_t buffer_size = 16;
+
+static void write(unsigned long num) {
+    unsigned int n = 1;
+    for (unsigned long i = num; i > 9; i /= 10, n *= 10);
+    for (; n > 0; n /= 10) {
+        STREAM.write(48 + (byte) ((num / n) % 10));
         STREAM.delay(10);
-    }
-    return lastRead;
-}
-
-static String read_line() {
-    String cmd;
-    char c = read_char();
-    while (c != '\n' && c != '\0') {
-        cmd += c;
-        c = read_char();
-    }
-    return cmd;
-}
-
-static void blink_led(const uint8_t pin, const unsigned long delay) {
-    digitalWrite(pin, HIGH);
-    STREAM.delay(delay);
-    digitalWrite(pin, LOW);
-}
-
-static void trigger_reset(const uint8_t pin, const unsigned long delay) {
-    digitalWrite(pin, HIGH);
-    STREAM.delay(delay);
-    digitalWrite(pin, LOW);
-}
-
-static void print_status() {
-    STREAM.println(F(" START                start the watchdog"));
-    STREAM.println(F(" STATUS               display the current watchdog status"));
-    STREAM.println(F(" STOP                 stop the watchdog"));
-}
-
-static void set_options(const String &command) {
-    if (command.startsWith("DELAY ", 4) && command.length() > 10) {
-        String rest = command.substring(10);
-        long long_delay = rest.toInt();
-        if (long_delay >= 1 && long_delay <= 65535) {
-            parmDelay = (unsigned long) long_delay * TO_SECONDS;
-        }
     }
 }
 
 void loop() {
-    const String command = read_line();
-    if (command.length() > 0) {
+    char command[buffer_size] = {'\0'};
+    int pos = 0, lastRead;
+    // when there are no characters to read, or the character isn't a newline
+    while (STREAM.available() && pos < buffer_size) {
+        lastRead = STREAM.read();
+        if (lastRead == '\0') {
+            break;
+        }
+        command[pos++] = (char) lastRead;
+        STREAM.delay(10);
+    }
+    if (pos > 0) {
         boolean valid_command = true;
-        if (command.equals("PING")) {
-            blink_led(LED_PIN, 100);
+        if (startTime > 0 && strcmp(command, "PING") == 0) {
+            toggle(LED_PIN, TOGGLE_DELAY);
             lastPing = millis();
-        } else if (command.startsWith("REBOOT ")) {
-            lastPing = 0;
-        } else if (command.startsWith("SET ")) {
-            set_options(command);
-        } else if (command.equals("START")) {
+        } else if (startTime == 0 && pos > 4 && strncmp(command, "SET ", 4) == 0) {
+            parmDelay = 1;
+            for (int i = 1; pos - i > 3; i++) {
+                parmDelay += pow(10, i - 1) * (command[pos - i] - 48);
+                STREAM.delay(10);
+            }
+        } else if (startTime == 0 && strcmp(command, "START") == 0) {
             startTime = millis();
-        } else if (command.equals("STATUS")) {
-            print_status();
-        } else if (command.equals("STOP")) {
+        } else if (strcmp(command, "STATUS") == 0) {
+            STREAM.write("DELAY: ");
+            write(parmDelay);
+            STREAM.write('\n');
+            STREAM.write("ARMED: ");
+            STREAM.write(startTime ? "True" : "False");
+            STREAM.write('\n');
+        } else if (startTime > 0 && strcmp(command, "STOP") == 0) {
             startTime = 0;
         } else {
             valid_command = false;
         }
 
         if (valid_command) {
-            STREAM.print(F("OK: "));
+            STREAM.write("OK:");
         } else {
-            STREAM.print(F("Unknown or invalid command: "));
+            STREAM.write("Unknown or invalid command: ");
         }
-        STREAM.println(command);
+        STREAM.write(command);
+        STREAM.write((byte) '\0');
     }
 
     if (startTime > 0 && millis() > lastPing + parmDelay) {
-        trigger_reset(RESET_PIN, 100);
+        STREAM.delay(10);
+        toggle(RESET_PIN, TOGGLE_DELAY);
         startTime = 0;
     }
     STREAM.refresh();
