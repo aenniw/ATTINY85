617c2b1c719bf0b76dd3041f08aa1b493c36eeb4
Martin Mihálek <aenniw@gmail.com>
2018-01-06 12:40:18 +0100

    Fixed reading of commands in HW watchdog
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- HW-Watchdog/src/watchdog.cpp
+++ HW-Watchdog/src/watchdog.cpp
@@ -32,27 +32,20 @@ static void toggle(const uint8_t pin, unsigned long delay_time) {
 
 const uint8_t buffer_size = 16;
 
-static void write(unsigned long num) {
-    unsigned int n = 1;
-    for (unsigned long i = num; i > 9; i /= 10, n *= 10);
-    for (; n > 0; n /= 10) {
-        STREAM.write(48 + (byte) ((num / n) % 10));
-        STREAM.delay(10);
-    }
-}
-
 void loop() {
     char command[buffer_size] = {'\0'};
     int pos = 0, lastRead;
-    // when there are no characters to read, or the character isn't a newline
-    while (STREAM.available() && pos < buffer_size) {
-        lastRead = STREAM.read();
-        if (lastRead == '\0') {
-            break;
+    while (pos + 1 < buffer_size) {
+        if (STREAM.available()) {
+            lastRead = STREAM.read();
+            if (lastRead == '\0') {
+                break;
+            }
+            command[pos++] = (char) lastRead;
         }
-        command[pos++] = (char) lastRead;
         STREAM.delay(10);
     }
+
     if (pos > 0) {
         boolean valid_command = true;
         if (startTime > 0 && strcmp(command, "PING") == 0) {
@@ -67,9 +60,6 @@ void loop() {
         } else if (startTime == 0 && strcmp(command, "START") == 0) {
             startTime = millis();
         } else if (strcmp(command, "STATUS") == 0) {
-            STREAM.write("DELAY: ");
-            write(parmDelay);
-            STREAM.write('\n');
             STREAM.write("ARMED: ");
             STREAM.write(startTime ? "True" : "False");
             STREAM.write('\n');
